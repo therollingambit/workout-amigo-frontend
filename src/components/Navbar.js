942f67d46878d66bb718d3b1dadd1b866be936e3
therollingambit <therollingambit@gmail.com>
2022-07-21 15:00:21 +0800

    add initial auth status

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,39 @@
-import { Link } from 'react-router-dom';
-import { useLogout } from '../hooks/useLogout';
+import { Link } from "react-router-dom";
+import { useAuthContext } from "../hooks/useAuthContext";
+import { useLogout } from "../hooks/useLogout";
 
 const Navbar = () => {
   const { logout } = useLogout();
+  const { user } = useAuthContext();
 
   const handleClick = () => {
     logout();
-  }
+  };
 
   return (
     <header>
       <div className="container">
-        <Link to='/'>
+        <Link to="/">
           <h1>Workout Buddy</h1>
         </Link>
 
         <nav>
-          <div>
-            <button onClick={handleClick}>Logout</button>
-          </div>
-          <div>
-            <Link to='/login'>Login</Link>
-            <Link to='/signup'>Sign up</Link>
-          </div>
+          {user && (
+            <div>
+              <span>{user.email}</span>
+              <button onClick={handleClick}>Logout</button>
+            </div>
+          )}
+          {!user && (
+            <div>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Sign up</Link>
+            </div>
+          )}
         </nav>
       </div>
     </header>
-  )
-}
+  );
+};
 
 export default Navbar;
